8fbc4dfb8ebb96dfee03f3a1b87792db5f22fae7
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-03 18:42:27 -0500

    update

--- test_wacky_beer.py
+++ test_wacky_beer.py
@@ -1,10 +1,4 @@
 import wacky_beer as wb
-import wacky_beer as wb
-import requests
-from bs4 import BeautifulSoup
-import json
 
 urls = wb.beer_styles_urls()
-soup = wb.bs_soup(urls[0][1])
-soup = BeautifulSoup(requests.get(urls[0][1]).content, 'html.parser')
-bs_id = json.loads(soup.find(id = 'wpp-json').get_text())['ID']
+bs_id, fobj = wb.bs_soup(urls[0][1])
