d7f9c87934a5dc411c0bf161b6a8bf5f5b1c2c5a
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-03 11:38:46 -0500

    building files

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='wacy_beer',
-    url='https://github.com/alejandroxag/wacky-beer-proj/wacky_beer',
+    url='https://github.com/alejandroxag/wacky-beer-proj/',
     author='Team Wacky',
-    author_email='alejandro.alvarez.1804@gmail.com',
+    author_email='',
     # Needed to actually package something
     packages=['wacky_beer'],
     # Needed for dependencies
     install_requires=[],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.1.0',
     # The license can be anything you like
     license='MIT',
     description='Group Project for the Data Focused Python Course. Beer information retrieval application.',
