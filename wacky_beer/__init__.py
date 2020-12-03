ccee6839729e19b7ffa1ef6cba76e88cf4fb4c55
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-03 13:45:19 -0500

    update

--- wacky_beer/__init__.py
+++ wacky_beer/__init__.py
@@ -1,8 +1,6 @@
 # __init__.py
 # wacky-beer package
-from .style_scrape import beer_styles_urls, bs_soup, bs_name, bs_desc, bs_cat, \
-    bs_color, bs_ibu, bs_pairings, bs_glassware, bs_temp, \
-    bs_features, bs_sugg, bs_getinfo
+from .style_scrape import beer_styles_urls, bs_soup, bs_name, bs_desc, bs_cat, bs_color, bs_ibu, bs_pairings, bs_glassware, bs_temp, bs_features, bs_sugg, bs_getinfo
 
 __version__ = '0.1.0'
 __author__ = 'Team Wacky'
