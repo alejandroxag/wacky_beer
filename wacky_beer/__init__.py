62f1545bf050d8774007bfd15c88d7777e6e2f27
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-04 02:14:25 -0500

    remove imports to bs_soup function body

--- wacky_beer/__init__.py
+++ wacky_beer/__init__.py
@@ -1,7 +1,8 @@
 # __init__.py
 # wacky-beer package
 from .style_scrape import beer_styles_urls, bs_soup, bs_name, bs_desc, bs_cat, bs_color, bs_ibu, bs_pairings, bs_glassware, bs_temp, bs_features, bs_sugg, bs_getinfo
-
+import json
+import re
 
 
 __version__ = '0.1.0'
