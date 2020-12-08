2f114d6a13de7ece5d825c4695b1b44a79cf17c0
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-08 14:07:54 -0500

    final version attempt 1

--- wacky_beer/__init__.py
+++ wacky_beer/__init__.py
@@ -2,8 +2,8 @@
 # wacky-beer package
 from .style_scrape import bs_urls, bs_soup, bs_name, bs_desc, bs_cat, bs_color, bs_ibu, bs_pairings, bs_glassware, bs_temp, bs_features, bs_sugg, bs_getinfo, bs_findurl
 from .styles_match import bs_relation, bs_search
-from beer_api import fetchBrew, findBrewery
-from kaggle_dataset import fetchBeerReviews
+from .beer_api import fetchBrew, findBrewery
+from .kaggle_dataset import fetchBeerReviews
 import json
 import re
 import requests
