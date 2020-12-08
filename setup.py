e3e7455618c7cfd9954caa5a98b49fcb75f0ea07
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-08 14:11:47 -0500

    attempt 2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # The license can be anything you like
     license='MIT',
     description='Group Project for the Data Focused Python Course. Beer information retrieval application.',
-    package_data={'': ['data/*.csv']},
+    package_data={'': ['data/beerstyles.csv', 'data/beer_merged_data.csv']},
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
